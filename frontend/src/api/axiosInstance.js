37b6855d28f17cad9a0db1b5361de7e7d2b3e823
Thalada Asrith Behala <uudoonasrith@gmail.com>
2025-07-03 05:51:32 +0530

    FIX: Correctly construct API base URL for deployment

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 import dayjs from 'dayjs';
 
-const baseURL = process.env.REACT_APP_API_BASE_URL;
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:8000';
 
 const axiosInstance = axios.create({
     baseURL,
